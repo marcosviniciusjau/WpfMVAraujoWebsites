4d36c15fc77e32f6745c1d397d6dd04fab11667f
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-14 12:31:40 -0300

    inicio do bd

--- View/UserControls/UcProdutos.xaml.cs
+++ View/UserControls/UcProdutos.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfAppSti3.View.UserControls;
+using WpfAppSti3.ViewModel;
 
 namespace WpfAppSti3.View.UserControls
 {
